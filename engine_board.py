4aa42c8135bbdc723b85de49f45df929d2e2a187
FallinAnno1154 <140545211+FallinAnno1154@users.noreply.github.com>
2024-02-02 20:52:15 +0300

    Update engine_board.py

--- engine_board.py
+++ engine_board.py
@@ -1,17 +1,17 @@
 class ChessEngine:
-def __init__(self):
-self.board = self.initialize_board()
+ def __init__(self):
+  self.board = self.initialize_board()
 
-def initialize_board(self):
-# Инициализация шахматной доски
-board = [[None] * 8 for _ in range(8)]
-# Назначение фигур:
-# Пешки черных
-for i in range(8):
-board[1][i] = Pawn("black")
-# Пешки белых
-for i in range(8):
-board[6][i] = Pawn("white")
+ def initialize_board(self):
+  # Инициализация шахматной доски
+  board = [[None] * 8 for _ in range(8)]
+  # Назначение фигур:
+  # Пешки черных
+  for i in range(8):
+   board[1][i] = Pawn("black")
+   # Пешки белых
+  for i in range(8):
+   board[6][i] = Pawn("white")
 # Ладьи
 board[0][0] = Rook("black")
 board[0][7] = Rook("black")
@@ -36,33 +36,33 @@ board[7][4] = Queen("white")
 return board
 
 def get_possible_moves(self, piece, x, y):
-# Получение всех возможных ходов для фигуры на заданных координатах
-possible_moves = []
+ # Получение всех возможных ходов для фигуры на заданных координатах
+ possible_moves = [
 
 if isinstance(piece, Pawn):
-# Логика ходов пешки
-# ...
+ # Логика ходов пешки
+ # ...
 
 elif isinstance(piece, Rook):
-# Логика ходов ладьи
-# ...
+ # Логика ходов ладьи
+ # ...
 
 elif isinstance(piece, Knight):
-# Логика ходов коня
-# ...
+ # Логика ходов коня
+ # ...
 
 elif isinstance(piece, Bishop):
-# Логика ходов слона
-# ...
+ # Логика ходов слона
+ # ...
 
 elif isinstance(piece, Queen):
-# Логика ходов ферзя
-# ...
+ # Логика ходов ферзя
+ # ...
 
 elif isinstance(piece, King):
-# Логика ходов короля
-# ...
-
+ # Логика ходов короля
+ # ...
+]
 return possible_moves
 
 def evaluate_board(self):
@@ -78,27 +78,27 @@ def make_move(self, move):
 # ...
 class Piece:
 def __init__(self, color):
-self.color = color
+ self.color = color
 class Pawn(Piece):
-def __init__(self, color):
-super().__init__(color)
+ def __init__(self, color):
+ super().__init__(color)
 class Rook(Piece):
-def __init__(self, color):
-super().__init__(color)
+ def __init__(self, color):
+  super().__init__(color)
 class Knight(Piece):
-def __init__(self, color):
-super().__init__(color)
+ def __init__(self, color):
+  super().__init__(color)
 class Bishop(Piece):
-def __init__(self, color):
-super().__init__(color)
+ def __init__(self, color):
+  super().__init__(color)
 class Queen(Piece):
-def __init__(self, color):
-super().__init__(color)
+ def __init__(self, color):
+  super().__init__(color)
 class King(Piece):
-def __init__(self, color):
-super().__init__(color)
+ def __init__(self, color):
+  super().__init__(color)
 # Пример использования шахматного движка
 if __name__ == "__main__":
-engine = ChessEngine()
-best_move = engine.get_best_move(3)
-engine.make_move(best_move)
+ engine = ChessEngine()
+ best_move = engine.get_best_move(3)
+ engine.make_move(best_move)
